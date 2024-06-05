74002b2f004def10b6057df30fe107737839fd76
learning-kyljmeeski <learning.kyljmeeski@gmail.com>
2024-06-05 09:21:31 +0600

    init

---
+++ src/main/java/com/kyljmeeski/vacancy_import/VacancyImportApplication.java
@@ -0,0 +1,13 @@
+package com.kyljmeeski.vacancy_import;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacancyImportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacancyImportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kyljmeeski/vacancy_import/VacancyImportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyljmeeski.vacancy_import;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacancyImportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
